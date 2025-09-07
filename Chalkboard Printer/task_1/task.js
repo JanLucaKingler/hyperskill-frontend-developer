4826c13e6d07997bcb5b28635f2c4bc4e2cbd0bf
Luca Kingler <152486882+JanLucaKingler@users.noreply.github.com>
2025-09-07 21:12:39 +0200

    Create task.js

---
+++ Chalkboard Printer/task_1/task.js
@@ -0,0 +1,7 @@
+const name  = "Bart";
+const surname = "Simpson";
+const message = "I will not skateboard in the halls.";
+
+for (let i = 0; i <= 4; i++) {
+    console.log(`This is ${name} ${surname} and ${message}`);
+}
